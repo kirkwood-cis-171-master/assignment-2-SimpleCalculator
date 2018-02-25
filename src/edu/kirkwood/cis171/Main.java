6c8db8580174eea44b234ec0e9f794b9cc42a9ad
BBrusegaard <benjaminbrusegaard@gmail.com>
2018-02-25 14:42:39 -0600

    Commiting final changes to SimpleCalculator

--- src/edu/kirkwood/cis171/Main.java
+++ src/edu/kirkwood/cis171/Main.java
@@ -4,12 +4,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String[] userInput;
-        userInput = scanInput();
+        String [] userInput = scanInput();
         try {
             System.out.print(userInput[0] + " " + userInput[1] + " " + userInput[2]);
         } catch (IndexOutOfBoundsException e) {
-            System.err.print("Invalid input: " + e.getMessage());
+            System.err.print("Invalid input: Please try again... ");
         }
         String endResult = calculation(userInput);
         outputResult(endResult);
@@ -18,40 +17,40 @@ public class Main {
     private static String[] scanInput() {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter a math problem involving operations: (+,-,/,*) to be solved:");
+        System.out.println("**Do not put spaces between operator and values... Ex; 8 + 8**");
         String calculation = s.nextLine();
-        String[] userInput;
-        userInput = calculation.split(" ");
+        String[] userInput = calculation.split("");
         return userInput;
     }
 
     public static String calculation(String[] operators) {
-        double firstNumber = 0.0;
-        double secondNumber = 0.0;
+        Double firstNumber = 0.0;
+        Double secondNumber = 0.0;
         String operatorType = " ";
         try {
             firstNumber = Double.parseDouble(operators[0]);
             secondNumber = Double.parseDouble(operators[2]);
             operatorType = operators[1];
         } catch (Exception e) {
-            System.out.println("Invalid input! Please try again...");
+            System.out.println("Make sure not to put spaces in between your equation... Ex; 8+8");
             System.exit(0);
         } if (operatorType.equals("+")) {
-            return ("The answer is: " + (String.format("%.3g%n", firstNumber + secondNumber)));
+            return (" = " + (String.format("%.3g%n", firstNumber + secondNumber)));
 
         } else if (operatorType.equals("*")) {
-            return("The answer is: " + (String.format("%.3g%n", firstNumber * secondNumber)));
+            return(" = " + (String.format("%.3g%n", firstNumber * secondNumber)));
 
         } else if (operatorType.equals("-")) {
-            return("The answer is: " + (String.format("%.3g%n", firstNumber - secondNumber)));
+            return(" = " + (String.format("%.3g%n", firstNumber - secondNumber)));
 
         } else if (operatorType.equals("/")) {
             if (secondNumber == 0) {
-                return("ERROR: You cannot divide a number by 0! Please try again...");
+                return(" -> ERROR: You cannot divide a number by 0! Please try again...");
             }
-            return("The answer is: " + (String.format("%.3g%n", firstNumber / secondNumber)));
+            return(" = " + (String.format("%.3g%n", firstNumber / secondNumber)));
 
         } else {
-            return("ERROR: Invalid input! Please try again...");
+            return(" -> ERROR: Invalid input! Please try again...");
         }
     }
 
